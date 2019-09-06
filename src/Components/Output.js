87d498df8c564d7340bd4233b2a28a6b2f118829
Leon Linton <leon.linton@live.co.uk>
2019-09-06 23:28:11 +0100

    Removes reference to BMI class

--- src/Components/Output.js
+++ src/Components/Output.js
@@ -5,13 +5,13 @@ class Output extends Component {
     let height = this.props.data.height;
     let weight = this.props.data.weight;
     let bmi = this.props.data.bmi;
-    let bmiClass = this.props.data.bmiClass;
+    // let bmiClass = this.props.data.bmiClass;
     return (
       <div className="output">
         <h3>{height} cm</h3>
         <h3>{weight} kilos</h3>
         <h3>Your BMI is: {bmi}</h3>
-        <h3>You are considered: {bmiClass}</h3>
+        {/* <h3>You are considered: {bmiClass}</h3> */}
       </div>
     );
   }
